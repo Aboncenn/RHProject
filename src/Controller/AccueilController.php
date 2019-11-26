2b57143405f2f7da7e7c65768d87655ef40a696a
Romain Guarnotta <guarnotta.romain@gmail.com>
2019-11-26 10:52:22 +0100

    apache-pack for routes

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="accueil")
+     * @Route("/", name="accueil")
      */
     public function index()
     {
