7ea5ba247f74b34d427316e6eeedb23e3cfdeae5
Romain Guarnotta <guarnotta.romain@gmail.com>
2019-11-26 15:06:48 +0100

    Display login if not logged and logout if logged

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -14,6 +14,7 @@ class AccueilController extends AbstractController
     {
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'user' => $this->getUser()
         ]);
     }
     /**
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -33,6 +34,7 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+        return new RedirectResponse($this->router->generate('accueil'));
+        //throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
